33d282a30f936dbc0397a046082dbc599a39d193
juancazvid <jan@cazvid.com>
2025-08-05 11:47:18 -0600

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""LinkedIn Scraper Apify Actor Package"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__description__ = "Professional LinkedIn data extraction tool for Apify platform"
+
+# Import main components
+from .main import LinkedInScraperActor, main
+
+__all__ = ["LinkedInScraperActor", "main"]
